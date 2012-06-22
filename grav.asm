cc483e12187fdb81ee74b0ee76415437c7eccb67
Pedro Lacerda <kanvuanza@gmail.com>
2012-06-22 15:39:14 -0300

    promgrama 'grav' finalizado

--- grav.asm
+++ grav.asm
@@ -10,7 +10,6 @@
 global _start
 [section .text]
 _start:
-
 ;;; Pedir o nome do arquivo para gravação de coordenadas das localidades
 	;; Write prompt
     	push	_prompt1, STDOUT
@@ -26,9 +25,17 @@ _start:
     	je	quit
     	
 ;;; Criar o arquivo de saída com o nome fornecido
-;	push	S_IRUSR | S_IWUSR, _filename
-;	call	sys.creat
-;	jc	error
+	push	S_IRUSR | S_IWUSR, _filename
+	call	sys.creat
+	jc	error
+	mov	[_filehandle], eax
+
+next_locale:
+;;; Clear locale variable
+	mov	edi, _locale
+	mov	edx, 36
+	mov	eax, 0
+	rep	stosb
 
 ;;; Pedir o nome da localidade
 	;; Write prompt
@@ -45,39 +52,80 @@ _start:
 	je	quit
 
 ;;; Pedir a latitude da localidade em graus, minutos e orientação
+ask_latitude:
 	;; Write prompt
 	push	_prompt3, STDOUT
 	call	io.write
 	jc	error
 	
-	push	90, _locale + Locale.latitude, STDIN
-	call	geo.read_coordinate
+	;; Read coordiante
+	push	'n', 's', 90, _locale + Locale.latitude, STDIN
+	call	geo.read_coord
+	jc	error
+	;; Input error
+	jo	latitude_error
 	
-quit:
-	push	eax
-	call	sys.exit
-
-error:
-;	push	STDERR, _err
-;	call	io.write
-;	
-;	push	STDERR, eax
-;	call	io.writeln
-	
-	push	_err
+	;; Continue to longitude
+	jmp	ask_longitude
+latitude_error:
+	push	_err1, STDOUT
 	call	io.write
+	jc	error
+	jmp	ask_latitude
+
+;;; Pedir a longitude da localidade em graus, minutos e orientação
+ask_longitude:
+	;; Write prompt
+	push	_prompt4, STDOUT
+	call	io.write
+	jc	error
 	
-	push	1
+	;; Read coordinate
+	push	'e', 'w', 180, _locale + Locale.longitude, STDIN
+	call	geo.read_coord
+	jc	error
+	;; Input error
+	jo	longitude_error
+	
+	;; Continue to write file
+	jmp	write_file
+longitude_error:
+	push	_err1, STDOUT
+	call	io.write
+	jc	error
+	jmp	ask_longitude
+
+;;; Gravar um registro com as coordenadas da localidade.
+write_file:
+	push	36, _locale, dword [_filehandle]
+	call	sys.write
+	jc	error
+	
+	;; Ask next locale
+	jmp	next_locale
+
+quit:
+	mov	ebx, 0
+	jmp	terminate
+error:
+	mov	ebx, 1
+	push	_err0, STDERR
+	call	io.write
+terminate:
+	push	dword [_filehandle]
+	call	sys.close
+	
+	push	ebx
 	call	sys.exit
 
 
 [section .data]
-_err		db 10,27,"[1;31mError!",27,"[0m",10,0
+_err0		db 10,27,"[1;31mError!",27,"[0m",10,0
+_err1		db 27,"[1;31mDados inválidos!",27,"[0m",10, 0
 _prompt1	db "Arquivo de coordenadas, ou ENTER para abortar: ", 0
 _prompt2	db "Nome da localidade                           : ", 0
 _prompt3	db "Latitude <graus, minutos, orientação>        : ", 0
-_prompt4	db "Coordenadas <graus, minutos, orientação>     : ", 0
-_test		db "  	abc", 0
+_prompt4	db "Longitude <graus, minutos, orientação>       : ", 0
 
 [section .bss]
 _filename	resb 255
