b37fdc2d5fd436c4ea8257da1cfb75347711b84c
Pedro Lacerda <kanvuanza@gmail.com>
2012-06-22 15:34:49 -0300

    geo.read_coord completo

--- geo.asm
+++ geo.asm
@@ -1,5 +1,7 @@
 
 %include "macros.inc"
+%include "io.asm"
+%include "str.asm"
 %include "sys.asm"
 
 [section .bss]
@@ -7,11 +9,14 @@ _coord_buf	resb 255
 
 
 [section .text]
-PROC geo.read_coordinate, 0, 16
-	
+PROC geo.read_coord, 0, 24
 	%define $fp		[ebp + 8]
 	%define $locale		[ebp + 12]
 	%define $max_deg	[ebp + 16]
+	%define $coord1		[ebp + 20]
+	%define $coord2		[ebp + 24]
+	
+	push	esi, edi
 	
 	mov	esi, _coord_buf
 	mov	edi, $locale
@@ -24,33 +29,74 @@ PROC geo.read_coordinate, 0, 16
 	;; Parse degrees
 	push	esi
 	call	str.atoi
-	jc	.error
-
-	dec	edx
+	jc	.input_error
 	add	esi, edx
-	
+
 	;; Validade degrees
 	cmp	eax, $max_deg
-	jg	.error
+	jg	.input_error
 	mov	[edi + Coordinate.degrees], al
 	
-;	push	dword $max_deg
-;	call	sys.exit
-;	
-;	;; Parse minutes
-;	push	esi
-;	call	str.atoi
-;	jc	.error
-;	add	esi, edx
-;	
-;	;; Validade minutes
-;	cmp	eax, 59
-;	jg	.error
-;	mov	[edi + Coordinate.minutes], al
+	;; Parse minutes
+	push	esi
+	call	str.atoi
+	jc	.input_error
+	add	esi, edx
+	
+	;; Validade minutes
+	cmp	eax, 59
+	jg	.input_error
+	mov	[edi + Coordinate.minutes], al
+	
+	;; Skip spaces
+	push	esi
+	call	str.skip_spaces
+	jc	.input_error
+	mov	esi, eax
+	
+	;; Parse orientation
+	mov	al, [esi]
+	
+	;; Validade orientation
+	mov	bl, byte $coord1
+	mov	bh, byte $coord2
+	
+	cmp	al, bl
+	je	.data_ok
+	sub	bl, 'a' - 'A'
+	cmp	al, bl
+	je	.data_ok
+	
+	cmp	al, bh
+	je	.data_ok
+	sub	bh, 'a' - 'A'
+	cmp	al, bh
+	je	.data_ok
+	jmp	.input_error
+	
+	
+.data_ok:
+	;; Is orientation upper case?
+	cmp	al, 'a'
+	jl	.save_data
+	
+	;; Turn orientation lower case
+	sub	al, 'a' - 'A'
+
+.save_data:
+	mov	[edi + Coordinate.orientation], al
+	
+	clc
+	jmp	.quit
 .error:
-	push	-1
-	call	sys.exit
 	stc
+	jmp	.quit
+.input_error:
+	clc
+	mov	al, 127 ;
+	inc	al	; overflow, input error
+	jmp	.quit
 .quit:
+	pop	edi, esi
 	exit
 ENDPROC
