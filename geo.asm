811507d15af47d6f474fa4a7b40d70c428134679
Pedro Lacerda <kanvuanza@gmail.com>
2012-06-25 02:25:08 -0300

    created geo.ask_latitude

--- geo.asm
+++ geo.asm
@@ -1,17 +1,11 @@
-
 %include "macros.inc"
 %include "io.asm"
 %include "str.asm"
 
-[section .bss]
-_coord_buf	resb 255
-
-
 [section .text]
-
 ;;;
 ;;; geo.read_coord
-;;; 	Parse a line into a Coordinate structure.
+;;; 	Read a line and parse into a Coordinate structure.
 ;;; args:
 ;;;     + file descriptor
 ;;;     + pointer to structure
@@ -23,14 +17,14 @@ _coord_buf	resb 255
 ;;; err:
 ;;;	Overflow flag set on parser error. Carry flag set on other errors.
 ;;; 
-PROC geo.read_coord, 0, 24
+PROC geo.read_coord, 0, 20
 	%define $fp		[ebp + 8]
 	%define $locale		[ebp + 12]
 	%define $max_deg	[ebp + 16]
 	%define $coord1		[ebp + 20]
 	%define $coord2		[ebp + 24]
 	
-	push	esi, edi
+	push	esi, edi, ebx, edx
 	
 	mov	esi, _coord_buf
 	mov	edi, $locale
@@ -111,6 +105,109 @@ PROC geo.read_coord, 0, 24
 	inc	al	; overflow, input error
 	jmp	.quit
 .quit:
-	pop	edi, esi
+	pop	edx, ebx, edi, esi
 	exit
 ENDPROC
+
+
+;;;
+;;; geo.ask_locale
+;;; 	Ask the user for a locale.
+;;; args:
+;;;     + pointer to locale structure
+;;; ret:
+;;;     Nothing
+;;; err:
+;;;	Overflow flag set for no input given. Carry flag set on other errors.
+;;; 
+PROC geo.ask_locale, 0, 4
+	push	edi, ecx
+	
+	mov	edi, [ebp + 8]
+	
+	;;; Clean locale variable
+	mov	ecx, 36
+	mov	eax, 0
+	rep	stosb
+	sub	edi, 36
+
+	;;; Ask locale name
+	;; Write prompt
+	push	_geo_prompt1, STDOUT
+	call	io.write
+	jc	.error
+	;; Read locale name
+	push	30, edi + Locale.name, STDIN
+	call	io.readln
+	jc	.error
+	;; Quit function
+	cmp	eax, 1
+	je	.eoi
+
+.ask_latitude:
+	;; Write prompt
+	push	_geo_prompt2, STDOUT
+	call	io.write
+	jc	.error
+	
+	;; Read coordiante
+	lea	eax, [edi + Locale.latitude]
+	push	'n', 's', 90, eax, STDIN
+	call	geo.read_coord
+	jc	.error
+	
+	;; Input error
+	jo	.latitude_error
+	
+	;; Continue to longitude
+	jmp	.ask_longitude
+
+.latitude_error:
+	push	_geo_err1, STDOUT
+	call	io.write
+	jc	.error
+	jmp	.ask_latitude
+
+.ask_longitude:
+	;; Write prompt
+	push	_geo_prompt3, STDOUT
+	call	io.write
+	jc	.error
+	
+	;; Read coordinate
+	lea	eax, [edi + Locale.longitude]
+	push	'e', 'w', 180,  eax, STDIN
+	call	geo.read_coord
+	jc	.error
+	;; Input error
+	jo	.longitude_error
+	
+	;; Continue to write file
+	jmp	.quit
+	
+.longitude_error:
+	push	_geo_err1, STDOUT
+	call	io.write
+	jc	.error
+	jmp	.ask_longitude
+
+.eoi: ;End Of Input
+	clc
+	mov	al, 127 ;
+	inc	al	; set OF
+	jmp	.quit
+.error:
+	stc
+.quit:
+	pop	ecx, edi
+	exit
+ENDPROC
+
+[segment .data]
+_geo_err1	db 27,"[1;31mDados inválidos!",27,"[0m",10, 0
+_geo_prompt1	db "Nome da localidade                           : ", 0
+_geo_prompt2	db "Latitude <graus, minutos, orientação>        : ", 0
+_geo_prompt3	db "Longitude <graus, minutos, orientação>       : ", 0
+
+[section .bss]
+_coord_buf	resb 255
