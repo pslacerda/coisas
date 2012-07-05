5073eb68f342f6c79c87733924474c28b6b0b2cd
Pedro Lacerda <kanvuanza@gmail.com>
2012-07-05 02:18:24 -0300

    Major refactor.

--- bulk.asm
+++ bulk.asm
@@ -5,6 +5,8 @@
 %include "sys.asm"
 %include "term.asm"
 
+%include "helpers.asm"
+
 [section .text]
 
 ;;;
@@ -14,156 +16,159 @@
 ;;;	+ input filename
 ;;;     + origin
 ;;; 
-PROC modes.bulk, 8, 8
+PROC modes.bulk, 0, 8
 	
 	;parameters
 	%define $infilename	[ebp + 8]
 	%define $origin		[ebp + 12]
 
-	;variables
-	%define $inf		[ebp - 4]
-	%define $outf		[ebp - 8]
-	
-	;aliases 
-	%define $buf1		_bulk_buffer1
-	%define $buf2		_bulk_buffer2
-	
 	push	ebx
 	
-	;open input file
-	push	O_RDONLY, dword $infilename
+;;
+;;	EBX: Pointer to program header (fancy if STDOUT else plain)
+;;	ECX: Input file
+;;	EDX: Output file
+;;
+
+;; Open input file
+	push	O_RDONLY, dword [ebp + 8]
 	call	sys.open
 	jc	.error
-	mov	$inf, eax
+	mov	ecx, eax
+
+;; Determine output file
+	print	BULK_OUTPUT_FILE_PROMPT, STDOUT
 	
-	;write prompt
-	push	_bulk_prompt1, STDOUT
-	call	io.write
-	jc	.error
-	
-	;ask output file
-	push	255, dword $buf1, STDIN
+	;ask filename
+	push	255, _bulk_buf1, STDIN
 	call	io.readln
 	jc	.error
 	
-	;write to STDOUT if no filename given
-	mov	dword $outf, STDOUT
-	cmp	eax, 1
-	je	.clear_screen
+	;determine
+	cmp	eax, 1			; if no filename given
+	je	.write_to_STDOUT	; write to STDOUT
+	jmp	.write_to_file		; write to file otherwise
 	
+;; User choose to print results to STDOUT
+.write_to_STDOUT:
+	;save file descriptor
+	mov	edx, STDOUT
+	
+	;clear screen
+	call	term.clear
+	jc	.error
+	
+	;choose fancy header
+	mov	ebx, CALC_FANCY_PROGRAM_HEADER
+	
+	;continue
+	jmp	.write_program_header
+
+;; User coose to print results to some file
+.write_to_file:
 	;create output file
-	push	S_IRUSR | S_IWUSR, dword $buf1
+	push	S_IRUSR | S_IWUSR, _bulk_buf1
 	call	sys.creat
 	jc	.error
 	
 	;open output file
-	push	O_WRONLY, dword $buf
+	push	O_WRONLY, _bulk_buf1
 	call	sys.open
 	jc	.error
-	mov	$outf, eax
+	mov	edx, eax
 	
-	mov	ebx, _bulk_header1
-	jmp	.write_origin_header
-	
-.clear_screen:
-	call	term.clear
-	jc	.error
-	mov	ebx, _header1
-	
-.write_origin_header:
-	push	ebx, dword $outf
-	call	io.write
-	jc	.error
-	
-	push	_bulk_header2, dword $outf
+	;choose plain header
+	mov	ebx, CALC_PROGRAM_HEADER
+	jmp	.write_program_header
+
+;; Write program header
+.write_program_header:
+	push	ebx, edx
 	call	io.write
 	jc	.error
 
 ;; Print origin
-	;print
-	push	dword $origin, dword $outf
+	print	CALC_ORIGIN_HEADER, edx
+
+	;print origin
+	push	dword $origin, edx
 	call	geo.print_locale
 	jc	.error
 	
 	;print two new lines
-	mov	dword [$buf1], 10
-	push	dword $buf1, dword $outf
-	call	io.writeln
+	mov	dword [_bulk_buf2], (10 << 8) | 10
+	print	_bulk_buf2, edx
 
-;; Print destines header
-	push	_bulk_header3, dword $outf
-	call	io.write
-	jc	.error
+;; Print destination header
+	print	CALC_DESTINATION_HEADER, edx
 
 ;; Read and compute distances
 .distances_loop:
 	;read locale
-	push	dword $buf1, dword $inf
+	push	_bulk_buf1, ecx
 	call	geo.read_locale
 	jc	.error
 	cmp	eax, 36
 	jl	.close_files
-	
-	;compute distance
-	push	dword $buf1, dword $origin
-	call	geo.compute_distance
-	mov	ebx, eax
-	
+
 	;print locale
-	push	dword $buf1, dword $outf
+	push	_bulk_buf1, edx
 	call	geo.print_locale
 	jc	.error
+
+	;compute distance
+	push	_bulk_buf1, dword $origin
+	call	geo.compute_distance
 	
 	;convert distance to string
-	push	dword $buf1, ebx
+	push	_bulk_buf1, eax
 	call	str.itoa
 	jc	.error
 	
 	;format distance
-	push	' ', 18, dword $buf2
+	push	' ', 18, _bulk_buf2
 	call	str.fill
 	
-	push	dword $buf1, dword $buf2
+	push	_bulk_buf1, _bulk_buf2
 	call	str.rjust
 	jc	.error
 	
-	push	dword $buf2, dword $outf
+	push	_bulk_buf2, edx
 	call	io.writeln
 	
 	;loooop!
 	jmp	.distances_loop
-	
+
+;; Close files
 .close_files:
-	push	dword $inf
+	;close output file
+	push	edx
 	call	sys.close
 	jc	.error
 	
-	cmp	dword $outf, STDOUT
-	je	
-
-.ask_new_locale:
-		
+	cmp	ecx, STDOUT
+	je	.decide_
 	
+	;close input file
+	push	ecx
+	call	sys.close
+	jc	.error
+
 	clc
-	jmp	.quit
+	jmp	.exit
+
 .error:
 	stc
+
+.exit:
 .quit:
 	pop	ebx
 	exit
 ENDPROC
 
 [section .bss]
-_bulk_buffer1	resb 255
-_bulk_buffer2	resb 255
-
-[section .data]
-_bulk_header1	db "Universidade Federal da Bahia",10
-		db "MATA49 Programação de Software Básico",10,10
-		db "Cálculo de distâncias geodésicas",10,10,0
-_bulk_header2	db "Origem                        Latitude   Longitude",10,0
-_bulk_header3	db "Destino                       Latitude   Longitude   Distância (km)",10,0
-_bulk_prompt1	db "Arquivo para salvar relatório (↵ STDOUT): ",0
+_bulk_buf1	resb 255
+_bulk_buf2	resb 255
 
 %endif
 
